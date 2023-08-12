512dad6558cde0c497b78932bfb83c05d8b7efce
naveensony07 <naveensoni0900@gmail.com>
2023-08-12 13:49:35 +0530

    Add program print student information

---
+++ print student information.py
@@ -0,0 +1,10 @@
+name="naveen Sony"
+print("My name is",name)
+regno="12210854"
+print("My Registration number is",regno)
+CGPA="7.86"
+print("My CGPA is",CGPA)
+address="Azamgarh"
+print("I live at",address)
+DOB="10-07-2000"
+print("My Date of Birth is",DOB)
