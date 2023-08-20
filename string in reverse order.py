7333253ca42ee6e1a9153addae44f6f7b84b63a5
--global <116912198+naveensony07@users.noreply.github.com>
2023-08-20 11:30:12 +0530

    String print in reverse order

---
+++ string in reverse order.py
@@ -0,0 +1,28 @@
+
+ # *** method 1 *******
+
+s1="NAVEEN"
+l=len(s1)
+while(l>0):
+    print(s1[l-1],end=' ')
+    l=l-1
+
+
+
+ # *** Method 2 ******
+                
+s1="NAVEEN"
+R=""
+for i in s1:
+   R=i+R
+print(R)
+
+
+# *** Method 3 ******
+
+S="NAVEEN"
+R=S[::-1]
+print(R)
+
+               
+                
