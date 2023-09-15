80a24a877fab4d48e66dbd4dc0832925132b52f1
--global <116912198+naveensony07@users.noreply.github.com>
2023-09-15 20:52:00 +0530

    code

---
+++ checkerbaord using reshape.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+matrix = np.arange(64).reshape(8, 8)
+
+for i in range(len(matrix)):
+    for j in range(len(matrix[0])):
+        if i % 2 != 0:
+            matrix[i][j] %= 2
+        else:
+            if matrix[i][j] %2 == 0:
+                matrix[i][j] = 1
+            else:
+                matrix[i][j] = 0
+            
+
+
+print(matrix)
