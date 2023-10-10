7ecad3e68653c4aac4884cfa04454694b66c0408
--global <116912198+naveensony07@users.noreply.github.com>
2023-10-11 00:47:12 +0530

    modify code

--- change font and size and color.py
+++ change font and size and color.py
@@ -1,13 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
-x=np.array([80,85,95,65,84,12,35,52])
-y=np.array([250,350,541,854,325,321])
-font1={'family':'serif','color':'blue','size':20}
-font2={'family':'serif','color':'darked','size':15}
 
-plt.title("Sports Watch Data")
-plt.xlabel("Average Pulse")
-plt.ylabel("Calories Burnage")
-plt.plot(x,y)
+x = np.array([80, 85, 95, 65, 84, 12, 35, 52])
+y = np.array([250, 350, 541, 854, 325, 321, 120, 420])  # Added extra y value
+
+font1 = {'family': 'serif', 'color': 'blue', 'size': 20}
+font2 = {'family': 'serif', 'color': 'darkred', 'size': 15}  # Corrected color name
+
+plt.title("Sports Watch Data", fontdict=font1)  # Added fontdict argument
+plt.xlabel("Average Pulse", fontdict=font2)  # Added fontdict argument
+plt.ylabel("Calories Burnage", fontdict=font2)  # Added fontdict argument
+plt.plot(x, y)
 plt.grid()
 plt.show()
--- plot.py
+++ plot.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 xpoints=np.array([0,6])
-xpoints=np.array([0,250])
-plt.plot(xpoints.ypoints)
+ypoints=np.array([0,250])
+plt.plot(xpoints,ypoints)
 plt.show()
