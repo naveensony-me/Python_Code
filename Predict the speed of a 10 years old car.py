3e2a4c17560c199fe9ac9aa2b8a59377250fa828
--global <116912198+naveensony07@users.noreply.github.com>
2023-10-27 10:44:51 +0530

    predict the speed of 10 years old car

---
+++ Predict the speed of a 10 years old car.py
@@ -0,0 +1,8 @@
+from scipy import stats
+x=[5,6,7,8,9,2,1,3,11,12,13,16]
+y=[99,86,77,48,79,82,91,83,81,92,103,76]
+slope,intercept,r,p,std_err=stats.linregress(x,y)
+def myfunc(x):
+return slope * x + intercept
+speed=myfunc(10)
+print(speed)
